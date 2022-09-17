eae632c1b33325cb7014c73312465183bcec8cff
marsvillager <1968643693@qq.com>
2022-09-17 15:41:19 +0800

    format data

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from format import init
 if __name__ == '__main__':
     df_ent, df_ics, df_mob, df_pre = init()
 
-    print(df_ics.loc[df_ics['attack_name'].str.contains('Compromise', case=False)])
+    print(df_ics)
+
+    # print(df_ics.loc[df_ics['attack_name'].str.contains('Compromise', case=False)])
