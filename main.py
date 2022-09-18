a1781c0c3a6faf8a2a71b01a011bdc48f56e7861
marsvillager <1968643693@qq.com>
2022-09-18 16:01:57 +0800

    format data

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ if __name__ == '__main__':
     # df_mob.to_csv('out/df_mob.csv', index=False, mode='a')
     # df_pre.to_csv('out/df_pre.csv', index=False, mode='a')
 
-    print(df_ics)
-
+    # print(df_ics)
     # print(df_ics.loc[df_ics['attack_name'].str.contains('Compromise', case=False)])
 
     # 2. security rules metadata
     key = load_file("00927_Account_Disabled_on_Windows.yml")
 
-    # print(key)
+    # print(key['name'])
+    # print(key['remarks'])
 
-    # print(key["name"])
-
-    # print(key["remarks"])
+    keywords = key['name'].split(' ') + key['remarks'].split(' ')
+    # print(keyword)
 
     # 3、key2key map
+    for keyword in keywords:
+        print(keyword)
